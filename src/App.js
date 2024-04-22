ce98ada4ff5c8362abee3a51027f68c888c5bf69
Narasimha <narasimhacharithartha@gmail.com>
2024-04-22 19:18:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
         <ChatEngine 
             height="100vh"
             projectID="cf45f1d3-fe8b-45e3-ac2b-c380b6347f0d"
-            userName="Carol"
-            userSecret="123456"
+            userName="elin"
+            userSecret="123456789"
         />
     );
 }
